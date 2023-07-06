ce93caf36b2d0abc57ae6230daf6f5f1f0e8916d
sug1no <3293067+sug1no@users.noreply.github.com>
2023-07-06 09:24:21 +0900

    fix some build errors ...

--- test/flutter_sequencer_method_channel_test.dart
+++ test/flutter_sequencer_method_channel_test.dart
@@ -5,11 +5,14 @@ import 'package:flutter_sequencer/flutter_sequencer_method_channel.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
+  // ignore: omit_local_variable_types
   MethodChannelFlutterSequencer platform = MethodChannelFlutterSequencer();
+  // ignore: omit_local_variable_types
   const MethodChannel channel = MethodChannel('flutter_sequencer');
 
   setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+    TestDefaultBinaryMessengerBinding.instance?.defaultBinaryMessenger
+        .setMockMethodCallHandler(
       channel,
       (MethodCall methodCall) async {
         return '42';
@@ -18,7 +21,8 @@ void main() {
   });
 
   tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+    TestDefaultBinaryMessengerBinding.instance?.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, null);
   });
 
   test('getPlatformVersion', () async {
